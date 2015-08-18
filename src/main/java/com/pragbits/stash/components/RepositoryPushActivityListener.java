a86d1f8346b5f3000ad2d06afb25b23fa9d51cdb
gonglinjie <gonglinjie@worktile.com>
2015-08-18 16:30:57 +0800

    modify soy

--- src/main/java/com/pragbits/stash/components/RepositoryPushActivityListener.java
+++ src/main/java/com/pragbits/stash/components/RepositoryPushActivityListener.java
@@ -83,7 +83,7 @@ public class RepositoryPushActivityListener {
                         .commits()
                         .buildAbsolute();
 
-                String text = String.format("Push on `%s` by `%s <%s>`. See <%s|commit list>.",
+                String text = String.format("Push on `%s` by `%s <%s>`. See [%s|commit list].",
                         event.getRepository().getName(),
                         event.getUser() != null ? event.getUser().getDisplayName() : "unknown user",
                         event.getUser() != null ? event.getUser().getEmailAddress() : "unknown email",
